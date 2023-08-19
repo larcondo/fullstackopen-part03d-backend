64194bf56145168ec9c1fe1ff64810df0f71dbea
larcondo <larcondo90@gmail.com>
2023-08-19 16:24:23 -0300

    second modification part3.1

--- index.js
+++ index.js
@@ -27,6 +27,24 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
