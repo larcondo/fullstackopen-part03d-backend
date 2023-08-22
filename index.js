652bca11df90f7b1811930c762643776c0af27ba
larcondo <larcondo90@gmail.com>
2023-08-22 15:51:52 -0300

    add validation create and update note

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
   
   if (body.content === undefined) {
@@ -65,19 +65,21 @@ app.post('/api/notes', (req, res) => {
     important: body.important || false,
   })
   
-  note.save().then( savedNote => {
+  note.save()
+  .then( savedNote => {
     res.json(savedNote)
   })
+  .catch( error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate( req.params.id, note, { new: true })
+  const { content, important } = req.body
+  
+  Note.findByIdAndUpdate( 
+    req.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
   .then( updatedNote => {
     res.json(updatedNote)
   })
@@ -99,6 +101,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
